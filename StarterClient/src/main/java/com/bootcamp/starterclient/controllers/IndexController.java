3ddaa6362c03004fc69b2b068befc053922392d1
AdrianLeonRivera <56595479+AdrianLeonRivera@users.noreply.github.com>
2021-11-17 14:08:58 +0100

    Subida Starter

---
+++ StarterClient/src/main/java/com/bootcamp/starterclient/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.bootcamp.starterclient.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class IndexController {
+	
+	@Autowired
+	private Saludo saludo;
+
+
+	@GetMapping("/saludo")
+	public ResponseEntity<String> saludo() {
+		
+		return new ResponseEntity<String>(HttpStatus.OK).ok(saludo.mensajeSaludo());
+	}
+}
